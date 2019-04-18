8d508eb96d91da2675e24d24397b03ab0a05c413
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2019-04-18 15:29:08 -0400

    final commit

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -12,7 +12,14 @@ import { ModalService } from "../../services/modal/modal.service";
 })
 export class DashboardComponent implements OnInit {
 
+
   targets: Company[];
+  researching: Company[] = [];
+  pendingApproval: Company[] = [];
+  approved: Company[] = [];
+  declined: Company[] = [];
+
+
   currentUser: string;
 
   constructor(
@@ -20,39 +27,89 @@ export class DashboardComponent implements OnInit {
     private modalService: ModalService,
     private spinner: Ng4LoadingSpinnerService,
     private route: ActivatedRoute
-    ) {
-      this.spinner.show()
-     }
+  ) {
+    this.spinner.show()
+  }
 
-  getCompanies(){
+  getCompanies() {
     this.companiesSer.getCompanies()
-    .subscribe(data => {
-      this.targets = data
-      this.spinner.hide()
+      .subscribe(data => {
+        this.targets = data
+        this.spinner.hide()
       }
-    )
-   }
+      )
+  }
 
-   changeStatus(t: Company, status: string){
-     t['status'] = status
-   }
+  changeStatus(t: Company, status: string, oldStatus: string) {
 
-  openModal(t: Company){
+    let categoryArr = []
+
+    if(status === oldStatus){
+      return;
+    }
+
+    switch (oldStatus) {
+      case 'targets':
+        categoryArr = this.targets;
+        break;
+      case 'researching':
+        categoryArr = this.researching;
+        break;
+      case 'pendingApproval':
+        categoryArr = this.pendingApproval;
+        break
+      case 'approved':
+        categoryArr = this.approved
+        break;
+      case 'declined':
+        categoryArr = this.declined
+        break
+    }
+
+    switch (status) {
+      case "targets":
+        this.targets.push(t);
+        break;
+      case "researching":
+        this.researching.push(t);
+        break;
+      case "pendingApproval":
+        this.pendingApproval.push(t);
+        break;
+      case "approved":
+        this.approved.push(t);
+        break;
+      case "declined":
+        this.declined.push(t);
+        break;
+    }
+
+    for (let i = 0; i < categoryArr.length; i++) {
+      if (categoryArr[i] === t) {
+        categoryArr.splice(i, 1)
+      }
+    }
+    this.modalService.openModal(t, 'successModal')
+
+
+  }
+
+  openModal(t: Company, oldStatus: string) {
     this.modalService.openModal(t, 'optionModal').then(data => {
       if (!data) {
         return;
       } else {
-        this.changeStatus(t, data)
-        this.modalService.openModal(t, 'successModal')
+        this.changeStatus(t, data, oldStatus)
       }
     })
   }
 
-  getCurrentUser(){
+  getCurrentUser() {
+
     this.route.queryParams.subscribe(user => this.currentUser = user["username"])
   }
 
-  onOutputTarget(e: Company){
+  onOutputTarget(e: Company) {
     this.targets.unshift(e)
   }
 
